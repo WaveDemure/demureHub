b02fcfa6124523b254b8c34da717fe3d645e4297
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-11-04 01:37:32 -0500

    Games testing

---
+++ Ultraviolet-Static/public/assets/js/list.js
@@ -0,0 +1,13 @@
+const appList = [
+    {
+        name: "example.com",
+        link: "https://example.com",
+        on: false
+    },
+    {
+        name: "Monkey Mart",
+        link: "https://monkey-mart.io/iframe/",
+        image: "assets/imgs/mImgs/MM.jpeg",
+        on: true
+    }
+]
