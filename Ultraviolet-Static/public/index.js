68e9b1ff0c67898a56d773f5dfb315ccad620d23
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-11-05 11:54:07 -0600

    Update index.js

--- Ultraviolet-Static/public/index.js
+++ Ultraviolet-Static/public/index.js
@@ -4,8 +4,6 @@ const input = document.getElementById("uv-search-engine")
 form.addEventListener('submit', async event => {
     event.preventDefault();
 
-    registerSW();
-
     const url = search(address.value, searchEngine.value);
     location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
