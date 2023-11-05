a5c7d27b8ece5826a0eab4ed930828335192fd8e
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-11-05 16:40:02 -0600

    Delete Ultraviolet-Static/public directory

--- Ultraviolet-Static/public/index.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('form');
-const input = document.getElementById("uv-search-engine")
-
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-
-    const url = search(address.value, searchEngine.value);
-    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-});
