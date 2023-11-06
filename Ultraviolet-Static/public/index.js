4dbd9894287515a5dce73200fcf113c597819e45
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-11-05 18:56:57 -0600

    Update index.js

--- Ultraviolet-Static/public/index.js
+++ Ultraviolet-Static/public/index.js
@@ -1,11 +1,21 @@
-// Searching 
+"use strict";
 
-const form = document.querySelector("form");
-const searchEngine = document.querySelector("input");
+const form = document.getElementById("uv-form");
+const searchEngine = document.getElementById("uv-search-engine");
+const error = document.getElementById("uv-error");
+const errorCode = document.getElementById("uv-error-code");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  
+
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
