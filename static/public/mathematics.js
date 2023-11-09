52273cb70094f00ae1dd1c65666fa6bfd580cc27
6tu9id <86175413+6tu9idd@users.noreply.github.com>
2023-11-08 18:24:03 -0800

    Update mathematics.js

--- static/public/mathematics.js
+++ static/public/mathematics.js
@@ -32,14 +32,6 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.head.appendChild(link);
         doc.body.appendChild(iframe)
-        location.replace("https://www.nasa.gov/")
+        location.replace("https://www.canvas.infrastructure.com/")
     }
 }
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-    if(window.localStorage.getItem("v4Particles") == "true") {
-      const scr = document.createElement("script");
-      scr.src="/scripts/academia.js";
-      document.body.appendChild(scr);
-    }
-  });
